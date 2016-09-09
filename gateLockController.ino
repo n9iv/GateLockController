1bb07d4384e03238896e7177e848dd99c9ed5bc7
n9iv <einav.sharon@gmail.com>
2016-09-09 23:03:14 +0300

    Major refactoring (before writing to EEPROM)

--- gateLockController.ino
+++ gateLockController.ino
@@ -1,9 +1,24 @@
 #include <SoftwareSerial.h>
+#include <EEPROM.h>
 
 SoftwareSerial RFID(2,3);
+
 #define RFID_SIZE 14
-int c = 0;
-char currentValue[RFID_SIZE];
+
+struct RfidCode {
+  char v[RFID_SIZE];
+};
+void printCode(struct RfidCode& code) {
+  Serial.print("{");
+  for(int i=0; i<RFID_SIZE-1; i++) {
+    Serial.print(code.v[i], DEC);
+    Serial.print(",");
+  }
+  Serial.print(code.v[RFID_SIZE-1], DEC);
+  Serial.print("}");
+}
+
+
 
 void setup() {
   // put your setup code here, to run once:
@@ -11,17 +26,13 @@ void setup() {
   RFID.begin(9600);
 }
 
-// Returns true if the specified value is one of the codes in the EEPROM
-bool IsValidCode(char* v) {
-  return true;  
-}
-
-void checkCode(char* v) {
-  char expected[RFID_SIZE] = {57,49,66,67,49,66,3,2,49,48,48,48,50,54};
+// Reutnrs true if a and b are matched by byte comparison. 
+bool DoesCodesMatch(struct RfidCode& a, struct RfidCode& b) {
   int numberOfMatches = 0;
-  for(int i=0; i<=RFID_SIZE;i++)
-    if(v[i]==expected[i])
+  for(int i=0; i<=RFID_SIZE;i++) {
+    if(a.v[i]==b.v[i])
       numberOfMatches++;
+  }
 
   if(numberOfMatches<RFID_SIZE) {
     Serial.print("Number of matches: ");
@@ -29,22 +40,46 @@ void checkCode(char* v) {
   }
   else
     Serial.print("Full Match!!!!");
+
+  return numberOfMatches==RFID_SIZE;
 }
+/*
+// Returns true if the specified value is one of the codes in the EEPROM
+bool IsValidCode(struct RfidCode& codeFromReader) {
+  struct RfidCode codeFromEEPROM;
+  for(int addr=0; addr<EEPROM.length(); addr+=RFID_SIZE) {
+    EEPROM.get(addr, codeFromEEPROM);
+    if(DoesCodesMatch(codeFromReader, codeFromEEPROM)) {
+      Serial.print("Found valid code\n");
+      return true;  
+    }
+  }
+  return false;
+}
+*/
+// Returns true if the specified code is the controlling code (used for updating the list of valid codes)
+bool IsControlCode(struct RfidCode& codeFromReader) {
+  struct RfidCode expected = {57,49,66,67,49,66,3,2,49,48,48,48,50,54};
+  return DoesCodesMatch(codeFromReader, expected);
+}
+
+
+struct RfidCode r;
+int c = 0;
 
 void loop() {
   if(RFID.available() >0)
   {
     int i = RFID.read();
-    currentValue[c] = i;
+    r.v[c] = i;
     c++;
-    Serial.print(i, DEC);
-    Serial.print(" ");
     if(c==RFID_SIZE) {
-      checkCode(currentValue);
+      printCode(r);
+      if(IsControlCode(r))
+        Serial.print(" Found Controlling code" );
       Serial.print("\n");
       c=0;
       delay(150);
-    }
-    
+    }    
   }
 }
